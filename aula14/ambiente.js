150becb684994a851b4f115c58febff06a5c6405
Rodrigo Vargas <rodrigo.7855x@gmail.com>
2024-11-16 14:23:05 -0300

    exercicio javascript

---
+++ aula14/ambiente.js
@@ -0,0 +1,21 @@
+var c = 1
+
+/*
+do {
+    console.log(c)
+    c++
+} while (c <= 10)
+*/
+
+/*
+while (c<= 10) {
+    console.log(c)
+    c++
+}
+*/
+
+console.log('Vai começar...')
+for (var c = 1; c <= 10; c++) {
+    console.log(c)
+}
+console.log('FIM!!')
