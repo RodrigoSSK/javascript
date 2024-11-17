ae6fd9d70f9a8dfd943c7912575709a755cbe2e4
Rodrigo Vargas <rodrigo.7855x@gmail.com>
2024-11-17 12:33:59 -0300

    finalização do exercicio

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -2,7 +2,7 @@ function tabuada() {
     let num = document.getElementById('txtn')
     let tab = document.getElementById('seltab')
     if (num.value.length == 0) {
-        window.alert('Por favor, digite um número!')
+        window.alert ('[ERRO] Digite um número no campo para gerar a TABUADA.')
     } else {
         let n = Number(num.value)
         let c = 1
@@ -10,10 +10,9 @@ function tabuada() {
         while (c <= 10) {
             let item = document.createElement('option')
             item.text = `${n} x ${c} = ${n*c}`
-            item.value = `tab${c}`
+            tab.value = `tab${c}`
             tab.appendChild(item)
             c++
         }
     }
-    
 }
